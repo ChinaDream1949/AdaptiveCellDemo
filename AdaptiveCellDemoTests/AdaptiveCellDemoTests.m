7044eef9acbcb6c976eb0d1bb83baf94812c8fe5
MR_Show <1420218361@qq.com>
2018-06-26 10:42:55 +0800

    Initial commit

---
+++ AdaptiveCellDemoTests/AdaptiveCellDemoTests.m
@@ -0,0 +1,39 @@
+//
+//  AdaptiveCellDemoTests.m
+//  AdaptiveCellDemoTests
+//
+//  Created by TG_Boy on 2018/6/19.
+//  Copyright © 2018年 MR.Shaw. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface AdaptiveCellDemoTests : XCTestCase
+
+@end
+
+@implementation AdaptiveCellDemoTests
+
+- (void)setUp {
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
---
+++ Podfile
@@ -0,0 +1,18 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'AdaptiveCellDemo' do
+  # Uncomment the next line if you're using Swift or would like to use dynamic frameworks
+  # use_frameworks!
+
+  # Pods for AdaptiveCellDemo
+
+  pod 'Masonry'
+  pod 'PYPhotoBrowser' #图片预览
+  
+  target 'AdaptiveCellDemoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
